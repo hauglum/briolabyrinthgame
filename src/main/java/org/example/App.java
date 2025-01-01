5a40e86d59d141052338ec9c2f6c70edf9e73e1a
Sindre <sindrehauglum@gmail.com>
2025-01-01 10:20:16 +0100

    Initial commit: Labyrinth Game App

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import com.yoctopuce.YoctoAPI.*;
+
+/**
+ * Simple example of Yoctopuce library usage
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Yoctopuce command line example (" + YAPI.GetAPIVersion() + ")");
+        try {
+            // setup the API to use local VirtualHub
+            YAPI.RegisterHub("127.0.0.1");
+            System.out.println("Device list");
+            YModule module = YModule.FirstModule();
+            while (module != null) {
+                System.out.println(module.get_serialNumber() + " (" + module.get_productName() + ")");
+                module = module.nextModule();
+            }
+        } catch (YAPI_Exception ex) {
+            System.out.println("IO error:" + ex.getLocalizedMessage());
+            ex.printStackTrace();
+        }
+        YAPI.FreeAPI();
+    }
+}
