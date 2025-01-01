3a8ad27084ce420d56ab721857dd67e604cd2f08
Sindre <sindrehauglum@gmail.com>
2025-01-01 11:50:14 +0100

    adding a menu for the console app

--- src/main/java/no/hauglum/App.java
+++ src/main/java/no/hauglum/App.java
@@ -1,16 +1,65 @@
 package no.hauglum;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.yoctopuce.YoctoAPI.*;
+
+import java.util.Scanner;
+
 
 /**
  * Simple example of Yoctopuce library usage
  *
  */
+
 public class App
 {
-    public static void main( String[] args )
-    {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args ) {
+
         System.out.println("Yoctopuce command line example (" + YAPI.GetAPIVersion() + ")");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            displayMenu();
+            char choice = scanner.nextLine().toUpperCase().charAt(0);
+
+            switch (choice) {
+                case '1':
+                    optionOne();
+                    break;
+                case '2':
+                    optionTwo();
+                    break;
+                case '3':
+                    optionThree();
+                    break;
+                case 'Q':
+                    System.out.println("Exiting the application. Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+
+        scanner.close();
+
+    }
+
+    private static void displayMenu() {
+        System.out.println("\n--- Main Menu ---");
+        System.out.println("1. List modules connected");
+        System.out.println("2. Run pre made sequence for simple map");
+        System.out.println("3. Manually control the ball (WASD Controls)");
+        System.out.println("Q. Quit");
+        System.out.print("Choose an option: ");
+    }
+
+    private static void optionOne() {
         try {
             // setup the API to use local VirtualHub
             YAPI.RegisterHub("127.0.0.1");
@@ -21,9 +70,18 @@ public class App
                 module = module.nextModule();
             }
         } catch (YAPI_Exception ex) {
-            System.out.println("IO error:" + ex.getLocalizedMessage());
-            ex.printStackTrace();
+            logger.error("there is an error while reading the connected devices", ex);
         }
         YAPI.FreeAPI();
+    }
+
+    private static void optionTwo() {
+        System.out.println("You selected Option Two.");
+        // Add functionality for Option Two here
+    }
+
+    private static void optionThree() {
+        System.out.println("You selected Option Three.");
+        // Add functionality for Option Three here
     }
 }
